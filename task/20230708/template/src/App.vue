78ac5348611812fdb7b48d76f19c25ded77ec6af
humo-tech <yano.wni+git@gmail.com>
2023-07-20 15:29:57 +0000

    vite/webgl テンプレート作成

---
+++ task/20230708/template/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWebGL from './components/HelloWebGL.vue'
+</script>
+
+<template>
+  <HelloWebGL />
+</template>
+
+<style scoped>
+</style>
---
+++ task/20230708/template/src/components/HelloWebGL.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useWebGL } from '../composable/useWebGL';
+
+const canvas = ref(null)
+
+
+onMounted(() => {
+  console.log(canvas.value)
+  const { init } = useWebGL(canvas.value)
+  init()
+})
+
+</script>
+
+<template>
+  <canvas ref="canvas"></canvas>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
